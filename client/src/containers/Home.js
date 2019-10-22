b757e9dd6db3b1b3d466e08b56d87b19e82612d8
Brandon Fields <brandonfields10@icloud.com>
2019-10-22 19:12:55 -0400

    Created pages in containers, about to start on backend parts

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Drinks on Us</h1>
-                <Link to= "/" >View All Drinks</Link>
-                
-                <Link to="/collection" >View All Drinks</Link>
-
-                <Link to="/"> More to Come</Link>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Drinks on Us</h1>
+        <Link to="/">View All Drinks</Link>
+        <div>
+          <br/>
+        </div>
+        <Link to="/collection">Create A Drinks</Link>
+        <div>
+            <br/>
+        </div>
+        <Link to="/"> More to Come</Link>
+      </div>
+    );
+  }
 }
 
 export default Home;
