816b42787bb6c9d95eeb66d2c97f23a03e4c8c6f
Ryan Hart <ryan@Ryans-MacBook-Pro-2.local>
2019-11-09 01:56:39 -0500

    update buttons for order summary

--- client/src/containers/OrderSummary.js
+++ client/src/containers/OrderSummary.js
@@ -1,25 +1,18 @@
 import React, { Component } from "react";
 import axios from "axios";
 import CheckOutBtn from "../components/CheckoutBtn/CheckOutBtn";
-import { Button } from 'reactstrap'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { Button } from "reactstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
 // import { fab } from '@fortawesome/free-brands-svg-icons'
-import {
-  faSortUp,
-  faSortDown,
-} from '@fortawesome/free-solid-svg-icons'
+import { faSortUp, faSortDown } from "@fortawesome/free-solid-svg-icons";
 
 // import { fad } from '@fortawesome/pro-duotone-svg-icons'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-library.add(
- faSortUp,
- faSortDown
-)
+library.add(faSortUp, faSortDown);
 // import { Link } from "react-router-dom";
-let userId = ""
+let userId = "";
 
 class OrderSummary extends Component {
   state = {
@@ -39,7 +32,7 @@ class OrderSummary extends Component {
       .catch(err => {
         console.log(err);
       });
-      axios
+    axios
       .get(`/api/drinks/order-summary/${userId}`)
       .then(response => {
         console.log(response.data.data);
@@ -50,7 +43,6 @@ class OrderSummary extends Component {
       .catch(err => {
         console.log(err);
       });
-
   }
 
   removeDrink = event => {
@@ -80,8 +72,11 @@ class OrderSummary extends Component {
     }
   };
 
-  changeMeasure = event => {
-    const { name, id, value } = event.target;
+  changeMeasure (id,name,value){
+    // const { name, id, value } = event.target;
+    console.log(name);
+    console.log(id);
+    console.log(value);
     this.setState(state => {
       const list1 = state.drinks.map((item, j) => {
         if (parseInt(id) === parseInt(j)) {
@@ -119,16 +114,16 @@ class OrderSummary extends Component {
       order: this.state.drinks
     };
     console.log(newOrder);
-    
+
     axios
       .post("/api/drinks/order-summary", newOrder)
       .then(response => {
         console.log(response.data.data._id);
-        userId = response.data.data._id
+        userId = response.data.data._id;
       })
       .catch(err => {
         console.log(err);
-        alert("Failed to create: " + err.message)
+        alert("Failed to create: " + err.message);
       });
 
     this.state.drinks.map((drink, i) => {
@@ -141,17 +136,23 @@ class OrderSummary extends Component {
         .catch(err => {
           console.log(err);
           alert("Failed to create: " + err.message);
-      });
-    })      
+        });
+    });
     this.setState({ drinks: [] });
   };
 
   render() {
     return (
       <div>
-        {this.state.currentOrder.length > 0 ? (<div>
-        {this.state.currentOrder.map((order,index) => (
-          <div>{order.drinkName}</div>))}</div>) : (<div></div>)}
+        {this.state.currentOrder.length > 0 ? (
+          <div>
+            {this.state.currentOrder.map((order, index) => (
+              <div>{order.drinkName}</div>
+            ))}
+          </div>
+        ) : (
+          <div></div>
+        )}
         <h1>Edit Drinks</h1>
         {this.state.drinks.map((drink, index) => (
           <div className="row border">
@@ -168,37 +169,32 @@ class OrderSummary extends Component {
                 <div className="row">
                   <div className="col-md-8">{ingredient.name}</div>
                   <div className="col-md-1">
-                    <button> 
-                    <FontAwesomeIcon
-                      icon={faSortUp}
-                      size="2x"
-                      color="success"
-                      id={index}
-                      name={i}
-                      value="+"
-                      onClick={this.changeMeasure}
-                    /></button>
-                  
-                  
+                  <FontAwesomeIcon
+                        icon={faSortUp}
+                        size="2x"
+                        color="success"
+                        id={index}
+                        name={i}
+                        value="+"
+                        onClick={() => this.changeMeasure((index),(i),"+")}
+                      />
                   </div>
-                  <div className="col-md-2">{ingredient.measure}</div>
+                  <div className="col-md-1">{ingredient.measure}</div>
                   <div className="col-md-1">
-                    <button>
                       <FontAwesomeIcon
-                      icon={faSortDown}
-                      size="2x"
-                      color="success"
-                      id={index}
-                      name={i}
-                      value="-"
-                      onClick={this.changeMeasure}
-                    />
-                    </button>
+                        icon={faSortDown}
+                        size="2x"
+                        color="success"
+                        id={index}
+                        name={i}
+                        value="-"
+                        onClick={() => this.changeMeasure((index),(i),"-")}
+                      />
                   </div>
                 </div>
               ))}
             </div>
-            <div className="col-md-2">
+            <div className="col-md-1">
               <button
                 className=" btn btn-danger"
                 id={index}
